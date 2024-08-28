733c36a97b2998234dcdbb81ed854c70c1ef6377
samped <michaelsamuelpedro@gmail.com>
2024-08-28 14:39:24 +0100

    modification of the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Header from "../components/header"
 import Hero from "../components/Hero"
+import { useEffect, useState} from 'react'
 
 export default function Home() {
+
+  const [account, setAccount] = useState<string | null>(null); 
   return (
     <main>
-      <Header />
+      <Header account={account} setAccount={setAccount} />
       <Hero />
     </main>
   );
